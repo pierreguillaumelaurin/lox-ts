213412d1df3f7742c9302eae9507f1d3a290e0c0
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-03-16 15:49:09 -0400

    Add lint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+function allo(test: string) {
+  return test;
+}
+
+function testasdf(toi: number) {
+  if (toi > 3) return 3;
+  return toi;
+}
+
+function test(toi: number) {
+  if (toi > 3) return 3;
+  return toi;
+}
+
+allo("toi");
+test(2);
+
 console.log("Hello via Bun!");
