8973a48f4dac7e098de5b3820d6783dde1402c54
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-03-22 20:25:40 -0400

    Implement basic Scanner methods

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -1,5 +1,75 @@
+import type { Token } from "Token";
+import TokenType from "TokenType";
+
 export class Scanner {
-  constructor(program: string) {
-    console.log(program);
+  private source = "";
+  private tokens: Token[] = [];
+
+  private start = 0;
+  private current = 0;
+  private line = 1;
+
+  constructor(source: string) {
+    this.source = source;
+  }
+
+  scanTokens(): Token[] {
+    while (!this.isAtEnd()) {
+      this.start = this.current;
+      this.scanToken();
+    }
+
+    this.tokens.push();
+    return this.tokens;
+  }
+
+  private scanToken() {
+    const c = this.advance();
+    switch (c) {
+      case "(":
+        this.addToken(TokenType.LEFT_PAREN);
+        break;
+      case ")":
+        this.addToken(TokenType.RIGHT_PAREN);
+        break;
+      case "{":
+        this.addToken(TokenType.LEFT_BRACE);
+        break;
+      case "}":
+        this.addToken(TokenType.RIGHT_BRACE);
+        break;
+      case ",":
+        this.addToken(TokenType.COMMA);
+        break;
+      case ".":
+        this.addToken(TokenType.DOT);
+        break;
+      case "-":
+        this.addToken(TokenType.MINUS);
+        break;
+      case "+":
+        this.addToken(TokenType.PLUS);
+        break;
+      case ";":
+        this.addToken(TokenType.SEMICOLON);
+        break;
+      case "*":
+        this.addToken(TokenType.STAR);
+        break;
+    }
+  }
+
+  private advance() {
+    this.current++;
+    return this.source[this.current - 1];
+  }
+
+  private addToken(type: TokenType, literal?: unknown) {
+    const text = this.source.substring(this.start, this.current);
+    this.tokens.push({ type, lexeme: text, literal, line: this.line });
+  }
+
+  private isAtEnd() {
+    return this.current >= this.source.length;
   }
 }
