79c7196e72497e4cb4943416c0f5016918e5a0e7
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-04-13 15:21:44 -0400

    Add other expressions

--- src/Ast.ts
+++ src/Ast.ts
@@ -1,4 +1,48 @@
 import type { Token } from "./Token.ts";
 
-type BinaryExpr = { type: "Binary"; left: Expr; operator: Token; right: Expr };
-export type Expr = BinaryExpr;
+export type BinaryExpr = {
+  type: "BinaryExpr";
+  left: Expr;
+  operator: Token;
+  right: Expr;
+};
+export type GroupingExpr = { type: "GroupingExpr"; expression: Expr };
+export type CallExpr = {
+  type: "CallExpr";
+  callee: Expr;
+  paren: Token;
+  arguments: Expr[];
+};
+export type LiteralExpr = { type: "LiteralExpr"; value: unknown };
+export type LogicalExpr = {
+  type: "LogicalExpr";
+  left: Expr;
+  operator: Token;
+  right: Expr;
+};
+export type UnaryExpr = { type: "UnaryExpr"; operator: Token; right: Expr };
+export type VariableExpr = { type: "VariableExpr"; name: Token };
+export type AssignExpr = { type: "AssignExpr"; name: Token; value: Expr };
+export type GetExpr = { type: "GetExpr"; name: Token; object: Expr };
+export type SetExpr = {
+  type: "SetExpr";
+  object: Expr;
+  name: Token;
+  value: Expr;
+};
+export type ThisExpr = { type: "ThisExpr"; keyword: Token };
+export type SuperExpr = { type: "SuperExpr"; keyword: Token; method: Token };
+
+export type Expr =
+  | BinaryExpr
+  | GroupingExpr
+  | LiteralExpr
+  | UnaryExpr
+  | VariableExpr
+  | AssignExpr
+  | LogicalExpr
+  | CallExpr
+  | GetExpr
+  | SetExpr
+  | ThisExpr
+  | SuperExpr;
