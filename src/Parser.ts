3113da732248d15011f675886dce61df6e8c3cd0
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2025-05-19 12:56:10 -0400

    Run eslint and prettier

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,13 +1,16 @@
-import type { Expr } from "Ast";
-import type { Token } from "Token";
+import type { Expr, UnaryExpr } from "Ast";
+import ErrorHandler from "ErrorHandler";
+import type * as Token from "Token";
 import TokenType from "TokenType";
 
 class Parser {
-  private readonly tokens: Token[];
+  private readonly tokens: Token.Token[];
+  private errorHandler: ErrorHandler;
   private current = 0;
 
-  constructor(tokens: Token[]) {
+  constructor(tokens: Token.Token[]) {
     this.tokens = tokens;
+    this.errorHandler = new ErrorHandler();
   }
 
   private expression() {
@@ -28,37 +31,78 @@ class Parser {
   private comparison() {
     let expr = this.term();
 
-    while (this.match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL)) {
+    while (
+      this.match(
+        TokenType.GREATER,
+        TokenType.GREATER_EQUAL,
+        TokenType.LESS,
+        TokenType.LESS_EQUAL,
+      )
+    ) {
       const operator = this.previous();
       const right = this.term();
-      expr = {type: "BinaryExpr", left: expr, operator, right};
+      expr = { type: "BinaryExpr", left: expr, operator, right };
     }
 
     return expr;
   }
 
   private term() {
-      let expr = this.factor();
+    let expr = this.factor();
 
-      while (this.match(TokenType.MINUS, TokenType.PLUS)) {
-        const operator = this.previous();
-        const right = this.factor();
-        expr = {type: "BinaryExpr", left: expr, operator, right};
-      }
-
-      return expr;
+    while (this.match(TokenType.MINUS, TokenType.PLUS)) {
+      const operator = this.previous();
+      const right = this.factor();
+      expr = { type: "BinaryExpr", left: expr, operator, right };
     }
 
-  private factor() {
-   let expr = this.unary();
-   while (this.match(TokenType.SLASH, TokenType.STAR)) {
-     const operator = this.previous();
-     const right = this.unary();
-     expr = {type: "BinaryExpr", left: expr, operator, right};
-   }
-    
-   return expr;
+    return expr;
   }
+
+  private factor() {
+    let expr = this.unary();
+    while (this.match(TokenType.SLASH, TokenType.STAR)) {
+      const operator = this.previous();
+      const right = this.unary();
+      expr = { type: "BinaryExpr", left: expr, operator, right };
+    }
+
+    return expr;
+  }
+
+  private unary() {
+    if (this.match(TokenType.BANG, TokenType.MINUS)) {
+      const operator = this.previous();
+      const right: UnaryExpr = this.unary();
+      return { type: "UnaryExpr", operator, right };
+    }
+
+    return this.primary();
+  }
+
+  private primary() {
+    if (this.match(TokenType.FALSE))
+      return { type: "LiteralExpr", value: false };
+    if (this.match(TokenType.TRUE)) return { type: "LiteralExpr", value: true };
+    if (this.match(TokenType.NIL)) return { type: "LiteralExpr", value: null };
+
+    if (this.match(TokenType.NUMBER, TokenType.STRING))
+      return { type: "LiteralExpr", value: this.previous().literal };
+
+    if (this.match(TokenType.LEFT_PAREN)) {
+      const expr = this.expression();
+      this.consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+      return { type: "GroupingExpr", expr };
+    }
+  }
+
+  private consume(type: TokenType, message: string) {
+    if (this.check(type)) return this.advance();
+
+    this.error(this.peek(), message);
+  }
+
+  private error() {}
 
   private match(...types: TokenType[]) {
     for (const type of types) {
@@ -71,9 +115,7 @@ class Parser {
     return false;
   }
 
-  
-
- private check(type: TokenType) {
+  private check(type: TokenType) {
     if (this.isAtEnd()) {
       return false;
     }
@@ -84,7 +126,7 @@ class Parser {
     if (!this.isAtEnd()) {
       this.current++;
     }
-    return previous();
+    return this.previous();
   }
 
   private isAtEnd() {
@@ -92,11 +134,13 @@ class Parser {
   }
 
   private peek() {
-    return this.tokens[this.current] ?? { type: TokenType.EOF };
+    return this.tokens[this.current] ?? { type: TokenType.EOF, literal: null };
   }
 
   private previous() {
-    return this.tokens[this.current - 1] ?? { type: TokenType.EOF };
+    return (
+      this.tokens[this.current - 1] ?? { type: TokenType.EOF, literal: null }
+    );
   }
 }
 
